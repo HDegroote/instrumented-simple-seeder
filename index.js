d33b7d7649e8a4889bbae16c9a914206f214c7b2
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-03-27 10:15:07 +0100

    Cleanup trace+monkeyPatch logic + use normal hypercore dep

--- index.js
+++ index.js
@@ -49,60 +49,10 @@ function applyTracerMonkeyPatches () {
   // Ideally we'd just add a trace counter before calling, to keep
   // it updated as this method changes in Hypercore
   // But for now we replace it completely because we need more detailed tracing
-  // const original = Replicator.Peer.prototype._requestRangeBlock
-  /* Replicator.Peer.prototype._requestRangeBlock = function _requestRangeBlock (index, length) {
+  const originalRequestRangeBlock = Replicator.Peer.prototype._requestRangeBlock
+  Replicator.Peer.prototype._requestRangeBlock = function   _requestRangeBlockMonkeyPatch (...args) {
+    const self = this
     this.tracer.trace('_requestRangeBlock')
-    if (this.core.bitfield.get(index) === true || !this._hasTreeParent(index)) return false
-
-    this.tracer.trace('_requestRangeBlock_add-block-to-replicator')
-    const b = this.replicator._blocks.add(index, 0)
-    if (b.inflight.length > 0) return false
-
-    this.tracer.trace('_requestRangeBlock_make-request')
-    const req = this._makeRequest(index >= length, b.priority)
-
-    // If the request cannot be satisfied, dealloc the block request if no one is subscribed to it
-    if (req === null) {
-      b.gc()
-      return false
-    }
-
-    this.tracer.trace('_requestRangeBlock_adding-inflight')
-    this._sendBlockRequest(req, b)
-
-    // Don't think this will ever happen, as the pending queue is drained before the range queue
-    // but doesn't hurt to check this explicitly here also.
-    if (b.queued) b.queued = false
-    return true
-  } */
-
-  Replicator.Peer.prototype._requestRangeBlock = function (index, length) {
-    this.tracer.trace('_requestRangeBlock')
-    if (this.core.bitfield.get(index) === true || !this._hasTreeParent(index)) return false
-
-    this.tracer.trace('_requestRangeBlock_add-block-to-replicator')
-    const b = this.replicator._blocks.add(index, 0)
-    if (b.inflight.length > 0) return false
-
-    this.tracer.trace('_requestRangeBlock_make-request')
-    const req = this._makeRequest(index >= length, b.priority)
-
-    // If the request cannot be satisfied, dealloc the block request if no one is subscribed to it
-    if (req === null) {
-      this.tracer.trace('_requestRangeBlock_insta-gc')
-      b.gc()
-      return false
-    }
-
-    req.block = { index, nodes: 0 }
-
-    this.tracer.trace('_requestRangeBlock_adding-inflight')
-    b.inflight.push(req)
-    this._send(req)
-
-    // Don't think this will ever happen, as the pending queue is drained before the range queue
-    // but doesn't hurt to check this explicitly here also.
-    if (b.queued) b.queued = false
-    return true
+    return originalRequestRangeBlock.call(self, ...args)
   }
 }
