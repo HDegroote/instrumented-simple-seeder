8ac7d6d5ebbe2bf77f519deb720b7c7dd3c1f761
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-04-04 11:06:22 +0200

    Expose getDescriptionWaitMs as opt and default to 30s

--- run.js
+++ run.js
@@ -17,7 +17,8 @@ function loadConfig () {
     instrumentHost: process.env.INSTRUMENT_HOST || '127.0.0.1',
     detailedMetrics: process.env.DETAILED_METRICS?.trim().toLowerCase() === 'true',
     sLogInterval: parseInt(process.env.S_LOG_INTERVAL || 0), // No logging by default
-    maxPeers: parseInt(process.env.MAX_PEERS) || undefined // Note: breaks hyperswarm if set to null (expects either undefined or an int)
+    maxPeers: parseInt(process.env.MAX_PEERS) || undefined, // Note: breaks hyperswarm if set to null (expects either undefined or an int)
+    getDescriptionWaitMs: parseInt(process.env.GET_DESCRIPTION_WAIT_MS || 30000)
   }
 
   if (!config.seedListKey) throw new Error('SEED_LIST_KEY must be set')
