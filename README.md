9246eb5108c6d6575ab26fd9e9174002c1c83468
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-06-18 13:48:04 +0200

    Support debug build

--- README.md
+++ README.md
@@ -13,3 +13,22 @@ Can be deployed as a [Docker container](https://hub.docker.com/r/hdegroote/simpl
 Alternatively, from the CLI:
 
 `SEED_LIST_KEY=<SEED-LIST-KEY> instrumented-seeder`
+
+## Debug Builds
+
+Push a tag containing 'debug' to trigger a debug build, including repl-swarm and heapdump support:
+
+```
+git tag debug-memleak
+git push origin debug-memleak
+```
+
+Create a directory where the heapsnapshots can be stored, and make it writable by all users (or chown it to the docker user).
+
+Then run a command like
+
+```
+docker run --name qt --env INSTRUMENT=true --env SEEDER_HEAPDUMP_INTERVAL_MINUTES=60 SEEDER_SUPPORT_HEAPDUMPS=true --SEED_LIST_KEY=<...> --mount type=volume,source=qt-volume,destination=/home/seeder/store --mount type=bind,source=/home/hans/qt/heapdumps,destination=/tmp/heapdumps hdegroote/seeder:debug
+```
+
+(hourly heapdumps will be stored in `/home/hans/qt/heapdumps` in this example)
--- run.js
+++ run.js
@@ -5,6 +5,10 @@ const setupLogger = require('pino')
 const runSeeder = require('./index')
 
 function loadConfig () {
+  const heapdumpInterval = process.env.SEEDER_HEAPDUMP_INTERVAL_MINUTES
+    ? parseInt(process.env.SEEDER_HEAPDUMP_INTERVAL_MINUTES) * 60 * 1000
+    : null
+
   const config = {
     port: parseInt(process.env.DHT_PORT || 0),
     corestoreLoc: process.env.STORAGE || './corestore',
@@ -13,6 +17,8 @@ function loadConfig () {
     instrument: process.env.INSTRUMENT?.trim().toLowerCase() === 'true',
     trace: process.env.SEEDER_TRACE?.trim().toLowerCase() === 'true',
     repl: process.env.REPL?.trim().toLowerCase() === 'true',
+    supportHeapdumps: (process.env.SEEDER_SUPPORT_HEAPDUMPS || '').toLowerCase().trim() === 'true',
+    heapdumpInterval,
     instrumentPort: parseInt(process.env.INSTRUMENT_PORT || 0),
     instrumentHost: process.env.INSTRUMENT_HOST || '127.0.0.1',
     detailedMetrics: process.env.DETAILED_METRICS?.trim().toLowerCase() === 'true',
@@ -26,10 +32,14 @@ function loadConfig () {
   return config
 }
 
-const config = loadConfig()
-const logger = setupLogger(
-  { name: 'simple-seeder', level: config.logLevel }
-)
+async function main () {
+  const config = loadConfig()
+  const logger = setupLogger(
+    { name: 'simple-seeder', level: config.logLevel }
+  )
 
-logger.info('Starting the seeder')
-runSeeder(logger, config)
+  logger.info('Starting the seeder')
+  runSeeder(logger, config)
+}
+
+main()
