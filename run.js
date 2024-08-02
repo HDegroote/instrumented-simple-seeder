1f163f16e69aa8669d3c94582b93cc09ab70a834
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-08-02 17:02:47 +0200

    full analysis

--- run.js
+++ run.js
@@ -63,8 +63,22 @@ async function main () {
 function monkeyPatchBuffer (logger) {
   let bufCounter = 0
   const bufMap = new Map()
+  const slabToKeys = new Map()
+  const keyToSlab = new Map()
+
   const registry = new FinalizationRegistry((key) => {
-    // console.log('deleting from registry')
+    // console.log('deleting from registry', key)
+    const slab = keyToSlab.get(key)
+    // if (slab.byteLength > 10000) console.log(bigSlabsDel++, 'delete slab size', slab.byteLength)
+    keyToSlab.delete(key)
+    // console.log('slab', slab, slabToKeys)
+    const slabKeys = slabToKeys.get(slab)
+    // console.log(slabKeys.size)
+    slabKeys.delete(key)
+    // console.log('post del', slabKeys.size)
+    if (slabKeys.size === 0) {
+      slabToKeys.delete(slab)
+    }
     clearTimeout(bufMap.get(key))
     bufMap.delete(key)
     // console.log('cleaning up', entry, `key ${key})`)
@@ -72,50 +86,113 @@ function monkeyPatchBuffer (logger) {
 
   const originalAllocUnsafe = Buffer.allocUnsafe
 
+  const bigBufferCutoff = 4000
+  const msBeforeItIsALeak = 1000 * 60
+  const msStatsInterval = 1000 * 90
+
   const leakCounters = new Map()
-  setTimeout(() => {
-    // console.log('leak overview', leakCounters)
+  setInterval(() => {
     const leaks = []
-    for (const [location, { amount, leakedBytesPerEntry }] of leakCounters.entries()) {
-      leaks.push({
-        location, amount, leakedBytesPerEntry, totalLeakedBytes: amount * leakedBytesPerEntry
-      })
-      // logger.warn(`${amount} leaks of ${leakedBytesPerEntry} at ${location} (total: ${amount * leakedBytesPerEntry})`)
+    const bigBufferLeaks = []
+    for (const [location, { keys, bufferLengths, arrayBufferLengths }] of leakCounters.entries()) {
+      let amount = 0
+      let normalisedTotalLeakedBytes = 0
+      let total = 0
+
+      let bigBuffersAmount = 0
+      let bigBuffersTotalSize = 0
+
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i]
+
+        const slab = keyToSlab.get(key)
+        if (!slab) continue // Already cleaned up (TODO: cleanly)
+        // if (arrayBufferLengths[i] > 10000) console.log('found big slab', arrayBufferLengths[i], keyToSlab.get(key))
+        const totalSlab = arrayBufferLengths[i]
+        const ownSize = bufferLengths[i]
+        const slabLeak = totalSlab - ownSize
+
+        if (ownSize >= bigBufferCutoff) {
+          bigBuffersAmount++
+          bigBuffersTotalSize += ownSize
+        }
+        if (slabLeak > 0) {
+          amount++
+          const totalRetainers = slabToKeys.get(slab).size
+          total += slabLeak
+          normalisedTotalLeakedBytes += slabLeak / totalRetainers // TODO: entry-specific (not necessarily all equal)
+        }
+      }
+      // Normalise by nr other buffers still retaining the slab
+      if (amount > 0) {
+        leaks.push({
+          location, normalisedTotalLeakedBytes, amount, total
+        })
+      }
+
+      if (bigBuffersAmount > 0) {
+        bigBufferLeaks.push({ amount: bigBuffersAmount, totalSize: bigBuffersTotalSize, location })
+      }
     }
 
-    leaks.sort((e1, e2) => e1.totalLeakedBytes < e2.totalLeakedBytes ? 1 : e1.totalLeakedBytes > e2.totalLeakedBytes ? -1 : 0)
-    for (const { amount, leakedBytesPerEntry, location, totalLeakedBytes } of leaks) {
-      console.log(`${amount} leaks of ${leakedBytesPerEntry} (total: ${byteSize(totalLeakedBytes)}) at ${location}`)
+    let totalSlabLeaks = 0
+    console.log('Slab retainer leaks')
+    leaks.sort((e1, e2) => e1.normalisedTotalLeakedBytes < e2.normalisedTotalLeakedBytes ? 1 : e1.normalisedTotalLeakedBytes > e2.normalisedTotalLeakedBytes ? -1 : 0)
+    for (const { amount, total, normalisedTotalLeakedBytes, location } of leaks) {
+      totalSlabLeaks += normalisedTotalLeakedBytes
+      console.log(`${amount} leaks of avg (${byteSize(total / amount)}) (total: ${byteSize(normalisedTotalLeakedBytes)} normalised against retainers--summed total with full slabs: ${byteSize(total)}) at ${location}`)
     }
-  }, 1000 * 60 * 1)
+
+    let totalBigBufferLeaks = 0
+    console.log('Big buffer leaks')
+    bigBufferLeaks.sort((e1, e2) => e1.totalSize < e2.totalSize ? 1 : e1.totalSize > e2.totalSize ? -1 : 0)
+    for (const { amount, totalSize, location } of bigBufferLeaks) {
+      totalBigBufferLeaks += totalSize
+      console.log(`${amount} leaks of big buffers of avg size ${byteSize(totalSize / amount)} (total: ${byteSize(totalSize)}) at ${location}`)
+    }
+
+    console.log(`Total slab leaked bytes (normalised against retainers): ${byteSize(totalSlabLeaks)}`)
+    console.log(`Total big buffer leaked bytes: ${byteSize(totalBigBufferLeaks)}`)
+  }, msStatsInterval)
 
   Buffer.allocUnsafe = function allocUnsafeMonkeyPatch (...args) {
     // console.log('unsafe alloc buffer')
     const res = originalAllocUnsafe(...args)
 
-    if (res.buffer.byteLength > 10 * res.byteLength) {
+    if (res.byteLength >= bigBufferCutoff || res.buffer.byteLength >= 10 * res.byteLength) {
       const trace = (new Error()).stack
       const key = bufCounter++
+      const slab = res.buffer
+
+      keyToSlab.set(key, slab)
+      let slabKeys = slabToKeys.get(slab)
+      if (!slabKeys) {
+        slabKeys = new Set()
+        slabToKeys.set(slab, slabKeys)
+      }
+      slabKeys.add(key)
+
       const bufferLength = res.byteLength
       const arrayBufferLength = res.buffer.byteLength
-      const leakedBytesPerEntry = res.buffer.byteLength - res.byteLength
+      // const leakedBytesPerEntry = res.buffer.byteLength - res.byteLength
       const timeout = setTimeout(() => {
         const location = trace.split('\n').slice(2).join('\n')
         let current = leakCounters.get(location)
         if (current === undefined) {
           current = {
-            amount: 0,
-            bufferLength,
-            arrayBufferLength,
-            leakedBytesPerEntry
+            keys: [],
+            bufferLengths: [],
+            arrayBufferLengths: []
           }
           leakCounters.set(location, current)
         }
-        current.amount++
+        current.keys.push(key)
+        current.bufferLengths.push(bufferLength)
+        current.arrayBufferLengths.push(arrayBufferLength)
         bufMap.delete(key)
         // logger.warn(`location ${location}`)
         // logger.warn(`Possible memleak for buffer length ${res.buffer.byteLength} of ${res.byteLength}: ${trace} `)
-      }, 1000 * 15)
+      }, msBeforeItIsALeak)
       bufMap.set(key, timeout)
       registry.register(res, key)
     }
